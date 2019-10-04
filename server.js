e4bdfc17693f2552fc3fe06bf4707d0874e1d5b0
Shamila <shamila.m09@gmail.com>
2019-10-03 19:32:32 -0700

    cleanup

--- server.js
+++ server.js
@@ -2,44 +2,45 @@
 var express = require('express');
 var app = express();
 var bodyParser	= require('body-parser');
-var nodemailer = require('nodemailer');
+// var nodemailer = require('nodemailer');
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/client'));
 
-
+//deleted on oct3, 19
 // SMTP starts
-var smtpTransport = nodemailer.createTransport({
-	service: "gmail",
-	host: "smtp.gmail.com",
-	auth: {
-		user: "",
-		pass: ""
-	}
-});
+// var smtpTransport = nodemailer.createTransport({
+// 	service: "gmail",
+// 	host: "smtp.gmail.com",
+// 	auth: {
+// 		user: "",
+// 		pass: ""
+// 	}
+// });
 // SMTP ends
 
 //routing starts
 
-app.post('/contact-form', function(req,res){
-	var data = req.body;
+//deleted on oct 3, 19
+// app.post('/contact-form', function(req,res){
+// 	var data = req.body;
 
-	var mailOptions ={
-		from : data.contactName,
-		to : "shamila.m09@gmail.com", //msg destination
-		subject : 'Message from'+' '+data.contactName+', Email: '+ data.contactEmail,
-		text : data.contactMsg
-	}
-	// console.log(mailOptions);
-	smtpTransport.sendMail(mailOptions, function(error, response){
-		if(error){
-			// console.log(error);
-			res.end("error");
-		}else{
-			res.end("sent");
-		};
-	});
-});
+// 	var mailOptions ={
+// 		from : data.contactName,
+// 		to : "shamila.m09@gmail.com", //msg destination
+// 		subject : 'Message from'+' '+data.contactName+', Email: '+ data.contactEmail,
+// 		text : data.contactMsg
+// 	}
+	
+// 	smtpTransport.sendMail(mailOptions, function(error, response){
+// 		if(error){
+			
+// 			res.end("error");
+// 		}else{
+// 			res.end("sent");
+// 		};
+// 	});
+// });
 
 // app.listen(8000, function() {
 // 	console.log('Server is listening on port 8000');
